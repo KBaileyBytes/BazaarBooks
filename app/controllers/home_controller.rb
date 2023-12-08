8caf68996373e992763789f31b19310096fa4afa
Kyle Bailey <kylebailey336@gmail.com>
2023-12-08 12:19:16 -0600

    Modified book show page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,17 @@
 class HomeController < ApplicationController
   def index
     if params[:genre_id].present?
-      pp params[:genre_id]
       if params[:genre_id] == 0.to_s
         books = Book.all.order('RANDOM()')
+        @page_title = "All Books"
       else
         genre_id = params[:genre_id]
         books = Book.joins(:genres).where(genres: {id: genre_id })
+        @page_title = "Books in #{Genre.find(genre_id).name}"
       end
     else
       books = Book.all.order('RANDOM()')
+      @page_title = "All Books"
     end
 
     @books = books.page(params[:page]).per(20)
